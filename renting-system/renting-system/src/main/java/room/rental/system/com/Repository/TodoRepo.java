cc21c12fc201cef7db71443a7824ee4458f23b8b
shivkant patel <shivkantp159@gmail.com>
2024-11-16 20:18:45 +0530

    changes

--- renting-system/renting-system/src/main/java/room/rental/system/com/Repository/TodoRepo.java
+++
@@ -1,9 +0,0 @@
-package room.rental.system.com.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import room.rental.system.com.Entity.Todo;
-
-@Repository
-public interface TodoRepo extends JpaRepository<Todo ,Long> {
-}
